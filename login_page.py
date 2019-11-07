2240bff0f0c0b44c7fe71dd39a75959dcd9b3f2f
matthew Robinson <matthewrobinson@matthews-MacBook-Air-3.local>
2019-11-06 20:02:21 -0600

    next step is to build the python

--- login_page.py
+++ login_page.py
@@ -1,9 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by.By
+
+
 class LoginPage(self):
+
+	# Driver constructor
+	# can include multiple drivers for cross browser testing 
+	def __init__(self, driver):
+		self._driver = webdriver.Chrome()
+	# self._driver = webdriver.Safari()
+	# self._driver = webdriver.Firefox() 
+
+
+	# find the locator/selector id by using page inspect
 	# username_box
 	# password_box
 	# login_button
 
 	# login method
 
+
 	def login(self):
 		
+
+		username_input = driver.find_element_by_id()
+
+
+		password_input = driver.find_element_by_id()
+
+	def search(self):
+
+		find_by()
